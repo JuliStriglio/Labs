820f0ecf82fcf461281d09f785333b5434706cc4
Juli Striglio <juliss2001@hotmail.com>
2023-04-19 11:32:42 -0300

    ej4b

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4b/LabSintaxis4b/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un año: ");
+            string año = Console.ReadLine();
+            int anio = Convert.ToInt32(año);
+
+            if (anio % 4 == 0)
+            {
+                if (anio % 100 == 0)
+                {
+                    if (anio % 400 == 0)
+                    {
+                        Console.WriteLine("El año es bisiesto");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año NO es bisiesto");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("El año es bisiesto ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El año NO es bisiesto");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
