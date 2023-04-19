3543f6aa982f894655d913bf3e93155c7fb867e7
Juli Striglio <juliss2001@hotmail.com>
2023-04-19 11:42:08 -0300

    ej4c

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4c/LabSintaxis4c/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] numeros = new int[20];
+            numeros[0] = 1;
+            numeros[1] = 1;
+
+            for(int i=2; i<20; i++)
+            {
+                numeros[i] = (numeros[i - 1] + numeros[i - 2]);
+            }
+            foreach(int item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
