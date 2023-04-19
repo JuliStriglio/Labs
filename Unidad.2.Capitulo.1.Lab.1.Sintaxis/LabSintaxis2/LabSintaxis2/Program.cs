8ac863e378b6387425ead0f6aa0952541498c0cc
Juli Striglio <juliss2001@hotmail.com>
2023-04-19 11:20:35 -0300

    ej4

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/LabSintaxis2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra");
+            String inputTexto = Console.ReadLine();
+            if (inputTexto == String.Empty)
+            {
+                Console.WriteLine("No ingreso un tipo de dato string");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese una opcion");
+                Console.WriteLine("1- Mostrar frase en mayuscula ");
+                Console.WriteLine("2- Mostrar frase en minuscula");
+                Console.WriteLine("3- Mostrar cantidad de caracteres");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                String opcion = Console.ReadLine();
+
+                int opc;
+
+                if (opcion == "1")
+                {
+                    Console.WriteLine("Mayuscula");
+                }
+                if (opcion == "2"){
+                    Console.WriteLine("minuscula");
+                }
+                if (opcion == "3"){
+                    Console.WriteLine(inputTexto.Length);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4/LabSintaxis4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero 1");
+            string num1 = Console.ReadLine();
+            int n1 = Convert.ToInt32(num1);
+
+            Console.WriteLine("Ingrese el numero 2");
+            string num2 = Console.ReadLine();
+            int n2 = Convert.ToInt32(num2);
+
+            int suma = n1 + n2;
+
+            Console.WriteLine("La sumatoria del numero" + num1 + "y el numero " + num2 + "es " + suma);
+            Console.ReadKey();
+
+        }
+    }
+}
