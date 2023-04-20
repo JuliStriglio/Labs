225ba8865d38a8c1785a62c76430c6dfcb084b91
Juli Striglio <juliss2001@hotmail.com>
2023-04-20 18:31:13 -0300

    ej4e

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4e/LabSintaxis4e/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] meses = {"enero", "febrero", "marzo", "abril", "mayo","junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" };
+
+            Console.WriteLine("Ingrese el nombre de un mes del año");
+            String mes = Console.ReadLine();
+
+            int i = 1;
+
+
+            foreach (string item in meses)
+            {
+                if (mes == item)
+                {
+                    Console.WriteLine("Nombre del mes:  " + mes + " numero del mes:  " + i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
