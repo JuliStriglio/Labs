a1f203a88a42468a32243dbfb28d617a066df436
Juli Striglio <juliss2001@hotmail.com>
2023-04-25 15:51:16 -0300

    ej4f

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4h/LabSintaxis4h/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4h
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int intento = 0;
+            string clave = "clave";
+            string leer = "";
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese la clave ");
+                leer = Console.ReadLine();
+                intento++;
+
+                if (leer == clave)
+                {
+                    Console.WriteLine("CLave correcta ");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Clave incorrecta ");
+                }
+                if (intento == 4)
+                {
+                    Console.WriteLine("Intento numero 4, NO tiene mas intentos");
+                    Console.ReadKey();
+                    break;
+
+                }
+
+                
+            }
+            
+           
+
+        }
+    }
+}
