c91f865bcddef29a229726c713e37595f28f88f4
Juli Striglio <juliss2001@hotmail.com>
2023-05-31 09:21:49 -0300

    ok

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases4/Clases/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Persona
+    {
+        private string Nombre = ("Julieta");
+        private string Apellido = ("Striglio");
+        private int Edad = 22;
+        private int DNI = 43164308;
+
+
+        public Persona(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public Persona()
+        {
+        }
+
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Se creo un objeto ");
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine(" Su nombre completo es: " + Nombre + Apellido);
+        }
+
+        public void GetAge()
+        {
+            Console.WriteLine("Su edad es :" + Edad);
+        }
+
+
+
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases4/LabClases4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace LabClases4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona per = new Persona();
+            per.MostrarMensaje();
+            per.GetFullName();
+            per.GetAge();
+
+            Console.ReadKey();
+        }
+    }
+}
