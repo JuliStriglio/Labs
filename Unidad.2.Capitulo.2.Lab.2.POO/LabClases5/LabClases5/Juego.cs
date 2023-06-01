c5566561f895ef6ad4409374b1c338faaf978e89
Juli Striglio <juliss2001@hotmail.com>
2023-06-01 17:07:34 -0300

    ok

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases5/LabClases5/Juego.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClases5
+{
+    class Juego
+    {
+        public Juego()
+        {
+
+        }
+
+        public void ComenzarJuego()
+        {
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1 - ");
+                Console.WriteLine("2 - ");
+                Console.WriteLine("3 - SALIR ");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Ingrese la opcion : ");
+
+            }
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Seleccionaste la opcion 1");
+                    Jugada ju = new Jugada(10);
+                   
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("Seleccionaste la opcion 2");
+                    break;
+
+                case "3":
+                    Console.WriteLine("HASTA LUEGO !");
+                    salir = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Ingrese una opcion valida");
+                    break;
+            }
+
+            Console.WriteLine();
+
+        }
+
+        void CompararRecord()
+        {
+
+        }
+       void Continuar()
+        {
+
+        }
+      void PreguntarMaximo()
+        {
+
+        }
+
+       void PreguntarNumero()
+        {
+
+        }
+
+        int _record;
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases5/LabClases5/Jugada.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClases5
+{
+    class Jugada
+    {
+        
+      public Jugada(int maxNumero)
+        {
+            Random rnd = new Random();
+            maxNumero = rnd.Next(maxNumero);
+        }
+
+
+       public bool Adivina
+       {
+          get { return _adivino; }
+            set { _adivino = value; }
+       }
+
+       public int Intentos
+       {
+           get { return _intentos;  }
+            set { _intentos = value; }
+       }
+
+       public string Numero
+        {
+            get { return _numero; }
+            set { _numero = value; }
+        }
+
+
+
+        void Comparar()
+        {
+            Console.WriteLine("Ingresar el numero para comparar : ");
+           _numero = Console.ReadLine();
+           _intentos++;
+           int _num;
+           _num = Convert.ToInt32(_numero);
+
+            Jugada j = new Jugada(10);
+        
+}
+
+           
+
+
+
+
+        private string _numero; // campo privado
+        private int  _intentos ; // campo privado
+        private bool _adivino; // campo privado
+    }
+}
