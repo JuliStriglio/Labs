c41be75badbf1a7f34a2cf2255ec21c57623dbdb
Juli Striglio <juliss2001@hotmail.com>
2023-06-06 11:36:13 -0300

    ok

---
+++ Unidad.2.Extra.Lab.3.LINQ/Ej2LINQ/Ej2LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 5; i++) {
+
+                Console.WriteLine("Ingrese un numero ");
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            var l = from li in list
+                    where li > 20
+            select li;
+
+            foreach (int i in l)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
