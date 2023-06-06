497d181b9a136a05e9907fcdc8d42d37bdd05a37
Juli Striglio <juliss2001@hotmail.com>
2023-06-06 11:24:43 -0300

    ok

---
+++ Unidad.2.Extra.Lab.3.LINQ/Ej1LINQ/Ej1LINQ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1LINQ
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] list = { " Buenos aires", "Santa fe", "Tucuman", "Cordoba" , "San luis"};
+
+            var l = from p in list
+                    where p.StartsWith("S") || p.StartsWith("T")
+                    select p;
+
+            foreach(String s in l)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
