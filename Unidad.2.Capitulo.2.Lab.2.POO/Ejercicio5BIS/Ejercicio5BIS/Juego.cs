fdec963308f624fc6f27daddebfd135ea5f089ee
Juli Striglio <juliss2001@hotmail.com>
2023-06-06 12:30:35 -0300

    OK

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/Ejercicio5BIS/Ejercicio5BIS/Juego.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio5BIS
+{
+    public class Juego
+    {
+
+        private int _record;
+
+        public Juego()
+        {
+
+        }
+ 
+        public void ComenzarJuego()
+        {
+
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("Ingresar la opcion ");
+                Console.WriteLine("1- Realizar una partida ");
+                Console.WriteLine("1-  ");
+                Console.WriteLine("1-  ");
+                Console.WriteLine("4- SALIR ");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1": Console.WriteLine("Realizar una partida");
+                        Jugada jug = new Jugada(10);
+
+                     for(int i=0; i<=3; i++)
+                        {
+                            Console.WriteLine("Ingrese el numero que cree adivinar : ");
+                            jug._numero = Convert.ToInt32(Console.ReadLine());
+                            i++;
+
+                            if (jug._numero == jug.Numero)
+                            {
+                                Console.WriteLine("Adivino !!!");
+                                i = _record;
+                            }
+                           
+                        }
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Opcion 1");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Opcion 1");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Hasta luego");
+                        break;
+                }
+            }
+            
+            
+            
+            
+            
+            
+            
+            throw new System.NotImplementedException();
+        }
+
+        private void CompararRecord()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Continuar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PreguntarMaximo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PreguntarNumero()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/Ejercicio5BIS/Ejercicio5BIS/Jugada.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio5BIS
+{
+    public class Jugada
+    {
+        public int _adivino;
+        public int _intentos;
+        public int _numero;
+
+        public Jugada(int maxNumero)
+        {
+           
+                Random rnd = new Random();
+                Numero = rnd.Next(maxNumero);
+            
+        }
+        public int Adivino
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Intento
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Numero
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Comparar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
