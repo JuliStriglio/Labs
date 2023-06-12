f53a9e5029df973eaf78cb502ec6e59779af8b18
Juli Striglio <juliss2001@hotmail.com>
2023-06-12 16:20:56 -0300

    mod

--- Unidad.2.Extra.Lab.3.LINQ/Ej3LINQ/Ej3LINQ/Program.cs
+++ Unidad.2.Extra.Lab.3.LINQ/Ej3LINQ/Ej3LINQ/Program.cs
@@ -8,8 +8,44 @@ namespace Ej3LINQ
 {
     class Program
     {
+
+        class Ciudad
+         {
+
+             public string Nombre;
+             public int CodP;
+
+             public Ciudad(string nombre, int codP)
+             {
+                 Nombre = nombre;
+                 CodP = codP;
+             }
+         }
         static void Main(string[] args)
         {
+
+            List<Ciudad> ciudades = new List<Ciudad>();
+
+            Ciudad ciu1 = new Ciudad ("Santa fe", 2173);
+            ciudades.Add(ciu1);
+             Ciudad ciu2 = new Ciudad ("Rosario", 2000);
+            ciudades.Add(ciu2);
+             Ciudad ciu3 = new Ciudad ("Cordoba", 5000);
+            ciudades.Add(ciu3);
+           
+
+            var c = from ci in ciudades
+                    where ci.Nombre.StartsWith("R")
+            select ci;
+
+            foreach()
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
         }
     }
 }
