f2801ea397ff94bdd0e9f23dee7e5490c5151e71
Juli Striglio <juliss2001@hotmail.com>
2023-06-12 21:47:19 -0300

    ok

---
+++ Unidad.3.Lab.1.Login/Ejercicio2/Ejercicio2/formLogin.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Ejercicio2
+{
+    partial class formLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lnkOlvidepass = new System.Windows.Forms.LinkLabel();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido!\r\nPor favor digite su informacion de ingreso\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de usuario";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contrasena";
+            // 
+            // lnkOlvidepass
+            // 
+            this.lnkOlvidepass.AutoSize = true;
+            this.lnkOlvidepass.Location = new System.Drawing.Point(12, 249);
+            this.lnkOlvidepass.Name = "lnkOlvidepass";
+            this.lnkOlvidepass.Size = new System.Drawing.Size(106, 13);
+            this.lnkOlvidepass.TabIndex = 3;
+            this.lnkOlvidepass.TabStop = true;
+            this.lnkOlvidepass.Text = "Olvide mi contrasena";
+            this.lnkOlvidepass.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkOlvidepass_LinkClicked);
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(198, 96);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(198, 141);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 5;
+            this.txtpass.TextChanged += new System.EventHandler(this.txtpass_TextChanged);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(170, 196);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // formLogin
+            // 
+            this.AcceptButton = this.btnIngresar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 271);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.lnkOlvidepass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Ingreso";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel lnkOlvidepass;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
+
